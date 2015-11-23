d14b7769f21b128391fcaa752bc652755d7968ea
evelynkharisma <kharismaeve@gmail.com>
2015-11-23 14:59:22 +0700

    commit

--- app/src/main/java/com/segid/bnote/Semester1Fragment.java
+++ app/src/main/java/com/segid/bnote/Semester1Fragment.java
@@ -24,7 +24,33 @@ public class Semester1Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        populateButtons();
+
         return inflater.inflate(R.layout.fragment_help, container, false);
     }
+    private void populateButtons() {
 
+        TableLayout table = (TableLayout) getView().findViewById(R.id.folderButtonTable);
+        for (int row=0; row < NUM_ROWS; row++){
+            TableRow tableRow = new TableRow(getContext());
+            table.addView(tableRow);
+            tableRow.setLayoutParams(new TableLayout.LayoutParams(
+                    TableLayout.LayoutParams.MATCH_PARENT, 100
+            ));
+            for(int column=0; column < NUM_COLS; column++)
+            {
+                Button folderButton = new Button(getContext());
+                folderButton.setCompoundDrawablesWithIntrinsicBounds(R.drawable.icons30, 0, 0, 0);
+                folderButton.setText("      FOLDER"+row);
+                folderButton.setBackgroundColor(getResources().getColor(R.color.bnote_transparent_background));
+                folderButton.setGravity(Gravity.LEFT);
+                folderButton.setGravity(Gravity.CENTER_VERTICAL);
+                tableRow.addView(folderButton);
+            }
+            Space rowSpace = new Space(getContext());
+            rowSpace.setMinimumHeight(20);
+            table.addView(rowSpace);
+        }
+    }
 }
